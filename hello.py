989980c9175d6bdfdf24726dd9eb2d5084f1b912
Jamil <jamilshaikh07@gmail.com>
2019-10-03 18:56:36 +0530

    testing python1

--- hello.py
+++ hello.py
@@ -4,18 +4,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello Flask!"
+'''
 @app.route("/hello")
 def hello():
     return "Hello World!"
-
-@app.route("/members")
-def members():
-    return "Members"
-
-@app.route("/members/Jamil")
-def getMember(name):
-    return "Jamil"
-
+'''
 if __name__ == "__main__":
     app.run()
 
